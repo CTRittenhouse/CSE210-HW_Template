c5956e976a3ecfc041bd3358e806b030b29fd414
Clint Rittenhouse <rit19005@byui.edu>
2026-04-25 18:05:16 -0600

    Completed Prep 1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?  ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        if (number >= 90 && number <= 100)
+
+        {
+            Console.WriteLine("Your grade is an A! ");
+        }
+        else if (number >= 80 && number <= 89)
+        {
+            Console.WriteLine("Your grade is a B ");
+        }
+        else if (number >= 70 && number <= 79)
+        {
+            Console.WriteLine("Your grade is a C ");
+        }
+        else if (number >= 60 && number <= 69)
+        {
+            Console.WriteLine("Your grade is a F :( ");
+        }
+        else if (number >= 0 && number <= 59)
+        {
+            Console.WriteLine("Your grade is a B ");
+        }
+        else if (number > 100)
+        {
+            Console.WriteLine("Okay Big-Shot! Youre cool! ;) ");
+        }
+        else
+        {
+            Console.WriteLine("I didnt think that was possible... ");
+        }
+
+
     }
 }
